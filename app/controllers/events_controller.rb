7bd9057fc07936cfc36617a54ff71096a8f892c0
Francette <mkt.curlyaho@gmail.com>
2019-08-14 17:24:58 +0300

    rajout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,27 @@
 class EventsController < ApplicationController
   def index
+  	@event = Event.all
+  end
+  def new
+  	@event = Event.new
+  end
+  def create
+  	@event = Event.create(start_date:params[:start_date], 
+  							duration:params[:duration], 
+  							title:params[:title], 
+  							event_description:params[:event_description], 
+  							price:params[:price],
+  							location:params[:location])
+    if @event.save
+      flash[:success] = " 😇 Félicitation! vous venez de créer un nouveau Evénement! 👌"
+        redirect_to 'root_path'
+    else
+    flash[:failed] = "🤔 Attention!!! un des champs n'est pas valide. 🤓 Veuillez réessayer svp!"
+        render '/events/new'
+    end
+  end
+
+  def show
+  	@event = Event.find(params[:id])
   end
 end
